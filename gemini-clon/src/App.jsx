dfb22f4575a619ea10ab7f6475b08cdaf74c6e0c
NIBI <haase.sergio1@gmail.com>
2025-09-11 20:13:15 -0600

    Creacion del Proyecto AIClone React + Vite + Tailwind

---
+++ gemini-clon/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">Tailwind v3.4.17 funcionando 🎉</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
