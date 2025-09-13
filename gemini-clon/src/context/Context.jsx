f96195a36f1b6c0da9de5426290eadeb5062607f
NIBI <haase.sergio1@gmail.com>
2025-09-12 19:06:33 -0600

    Creacion seccion de Chats para interaccion con la IA

--- gemini-clon/src/context/Context.jsx
+++ gemini-clon/src/context/Context.jsx
@@ -1,25 +1,96 @@
 import { createContext, useState } from 'react';
+import main from "../config/gemini.js"
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
-    
+
     const [user, setUser] = useState(false);
-    const [chat, setChat] = useState([]); //Aqui se guardará el historial del chat actual
-    const [chatList, setChatList] = useState([]) //Aqui guardaremos el listado de los chats que se generen
+
+    //Estados para el uso de la IA
+    const [currentChat, setCurrentChat] = useState(0); //Estado que indica que chat se va a mostrar el el chat
+    const [chat, setChat] = useState([]); //Estado que despliega el historial del chat que se desplegó
+    const [chatList, setChatList] = useState([]); //Estado que guardará el listado de los chats que se generen
+    const [input, setInput] = useState("");  //Estado que guarda el prompt del usuario
+    const [recentPrompt, setRecentPrompt] = useState(""); //Estado que guarda El prompt mas reciente desplegado
+    const [showResult, setShowResult] = useState(false); //Estado que nos indica si se muestra la pantalla principal o un chat
+    const [loading, setLoading] = useState(false); //Estado que carga la animación de carga mientras se espera por una respuesta
+    const [resultData, setResultData] = useState(""); //Estado que muestra la respuesta más reciente
+
+    
+    //Función para la animación que muestra la respuesta de la IA
+    const delayData = (index, nextWord) => {
+        setTimeout(function () {
+            setResultData(prev=>prev+nextWord)
+        }, 75*index)
+    }
+
+    //Función que recibe el prompt del usuario y pide una respuesta de la API
+    const onSent = async () => {
+
+        //Agregamos lo más reciente de la conversación a nuestro historial del chat en caso de que sea una conversación activa
+        if (recentPrompt) {
+            setChat(prev => [...prev, {prompt: recentPrompt, response: resultData }]);
+
+        } 
+        
+        //Borramos el estado de la última respuesta del la IA
+        setResultData("");
+        //Iniciamos un nuevo cuadro con la animación loading
+        setLoading(true)
+        //Indicamos que se mostrara una conversación en caso de que no la hubiera antes
+        setShowResult(true)
+
+        //Guardamos el prompt del usuario
+        setRecentPrompt(input)
+
+        //Esperamos una respuesta de la IA y la guardamos en una constante
+        const response = await main(input)
+        
+        //Borramos el prompt actual
+        setInput("")
+
+        //Animación para mostrar la respuesta
+        let newResponse= response.split(" ");
+        for (let i = 0; i < newResponse.length; i++) {
+            const nextWord = newResponse[i];
+            delayData(i,nextWord+" ")
+        }
+
+        //Quitamos la animacíon de loading
+        setLoading(false)
+        
+    }
 
     //Estados para el diseño de la pagina
     const [sidebarWidth, setSidebarWidth] = useState(64); //Cambiará de acuerdo a los estados sel sidebar
     const [isPinned, setIsPinned] = useState(false); //Cambiara de acuerdo a los estados de sidebar
     const [darkMode, setDarkMode] = useState(false);
 
+
+    //Valores que mandaremos en el contexto
     const contextValue = {
+        //Estado de validación de usuario 
         user,
         setUser,
+        //Estados para el uso de la IA
+        onSent,
         chat,
         setChat,
         chatList, 
         setChatList,
+        currentChat,
+        setCurrentChat,
+        input,
+        setInput,
+        recentPrompt,
+        setRecentPrompt,
+        showResult,
+        setShowResult,
+        loading,
+        setLoading,
+        resultData,
+        setResultData,
         //Estados de diseño de página
         sidebarWidth,
         setSidebarWidth,
--- gemini-clon/src/pages/Chats.jsx
+++ gemini-clon/src/pages/Chats.jsx
@@ -1,10 +1,169 @@
+import { useRef, useEffect, useContext } from 'react';
+import { Context } from '../context/Context.jsx';
+import ReactMarkdown from 'react-markdown';
+import DOMPurify from 'dompurify';
+import remarkGfm from 'remark-gfm';
 
 const Chats = () => {
-    return(
-        <>
-            <h1>Chats</h1>
-        </>
-    )
+    const messagesEndRef = useRef(null);
+    const chatContainerRef = useRef(null);
+
+    const { onSent, chat, recentPrompt, showResult, loading, resultData, setInput, input, sidebarWidth, isPinned , darkMode } = useContext(Context);
+
+    // Efecto para hacer scroll al final cuando hay nuevos mensajes
+    useEffect(() => {
+        scrollToBottom();
+    }, [resultData, darkMode]);
+
+    //Guardamos el with del sidebar para usarlo en nuestro componente
+    const width = `${sidebarWidth}px`
+
+    //Función para mostrar lo último de la convesación al recibir una respuesta de la IA
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    //Funcion para pedir una respuesta de la IA
+    const handleSendMessage = async () =>{
+        if (input.trim() === '') return;
+        await onSent();
+    }
+
+    //Función para manejar la tecla ENTER
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            handleSendMessage();
+        }
+    };
+
+    return (
+        <div 
+        id='dashboard'
+        className="flex flex-col items-center h-full bg-white dark:bg-gray-950 dark:text-gray-200 overflow-hidden"
+        style={!isPinned ? ({marginLeft: "64px"}) : ({marginLeft: width})}
+        >
+            {/* Área de mensajes */}
+            <div 
+            ref={chatContainerRef}
+            className="flex-1 flex justify-center w-full overflow-y-auto transition-all duration-300 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent hover:scrollbar-thumb-gray-400"
+            >
+                <div 
+                className='flex justify-center p-4' 
+                id='chatContainer'
+                >
+                    {!showResult ? (
+                        <div className="flex items-center justify-center w-full h-full">
+                            <h2 className="text-6xl text-center font-medium">Descubre Rebel, tu asistente de IA personal</h2>
+                        </div>
+                    ) : (
+                        <div className="space-y-4 w-full h-full">
+                            {chat.map((interaction, index)=>{
+                                return(
+                                    <>
+                                        <div 
+                                        className='flex justify-end'
+                                        key={index+"userPrompt"}
+                                        >
+                                            <div className='flex max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl rounded-2xl rounded-tr-sm p-3 bg-slate-400'>
+                                                <span className="break-words mt-1">
+                                                    <ReactMarkdown
+                                                    remarkPlugins={[remarkGfm]}
+                                                    >
+                                                        {DOMPurify.sanitize(interaction.prompt)}
+                                                    </ReactMarkdown>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div 
+                                        className='flex justify-start' 
+                                        key={index+"aiResponse"}
+                                        >
+                                            <div className='flex max-w-xs md:max-w-md lg:max-w-2xl xl:max-w-xl p-3 dark:bg-gray-950 bg-white h-fit'>
+                                                <span className="text-2xl text-blue-500">
+                                                    <i className="fa-brands fa-fulcrum mr-3"></i>
+                                                </span>
+                                                <div className='result-data w-full'>
+                                                    <ReactMarkdown
+                                                    remarkPlugins={[remarkGfm]}
+                                                    >
+                                                        {DOMPurify.sanitize(interaction.response)}
+                                                    </ReactMarkdown>
+                                                    <div ref={messagesEndRef} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </>
+                                )
+                            })}
+                            <div className='flex justify-end' >
+                                <div className='flex max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl rounded-2xl rounded-tr-sm p-3 bg-slate-400'>
+                                    <span className="break-words mt-1">{recentPrompt}</span>
+                                </div>
+                            </div>
+                            <div className='flex justify-start' >
+                                <div className='flex max-w-xs md:max-w-md lg:max-w-2xl xl:max-w-xl p-3 bg-white'>
+                                    <span className={`text-2xl text-blue-500 ${loading ? "rounded-full animation" : ""}`}>
+                                        <i className={`fa-brands fa-fulcrum  ${!loading ? "mr-3": "rounded-full bg-white py-1 px-2"} `}></i>
+                                    </span>
+                                    {loading ? (
+                                        <span className='mt-1'></span>
+                                        ):(
+                                            <div className='result-data'>
+                                                <ReactMarkdown
+                                                remarkPlugins={[remarkGfm]}
+                                                >
+                                                    {DOMPurify.sanitize(resultData)}
+                                                    
+                                                </ReactMarkdown>
+                                                <div ref={messagesEndRef} />
+                                            </div>
+                                        ) }
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            {/* Área de entrada de texto */}
+            <div className="flex flex-col justify-center items-center bg-white dark:bg-gray-950 dark:text-gray-200 p-4 w-full">
+                <div 
+                className="flex items-center border border-gray-300 rounded-3xl py-2 px-4 items-center m-2"
+                id='text-container'
+                >
+                    <button className="px-2 py-1 rounded-full bg-gray-100 dark:bg-gray-800 dark:text-gray-200 hover:bg-gray-200 transition-colors mr-2">
+                        <i className="fa-solid fa-plus font-light"></i>
+                    </button>
+
+                    <div className="flex-1 relative">
+                        <textarea
+                        id='input-container'
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        onKeyDown={handleKeyPress}
+                        placeholder="Pregunta a Rebel"
+                        className="w-full py-2 px-4 pr-10 mt-1 resize-none dark:bg-gray-800 focus:outline-none focus:ring-none focus:border-transparent"
+                        rows="1"
+                        style={{ minHeight: '44px', maxHeight: '120px' }}
+                        />
+                    </div>
+
+                    <button
+                    onClick={handleSendMessage}
+                    disabled={input.trim() === ''}
+                    className={`py-1 px-2 rounded-full ml-2 transition-colors dark:bg-gray-800 dark:text-gray-200 ${input.trim() === '' ? 'bg-gray-200 text-gray-400' : 'bg-blue-500 text-white hover:bg-blue-600'}`}
+                    >
+                        ➤
+                    </button>
+                </div>
+        
+                <div className="flex justify-between items-center mt-2">
+                    <p className="text-xs text-gray-500">Se aplican términos y condiciones</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Chats
