48d4c33197d5253ddf85664a8794f864316a8ea3
NIBI <haase.sergio1@gmail.com>
2025-09-15 15:12:01 -0600

    Animaciones de entrada de página

--- gemini-clon/src/routes/AppRoutes.jsx
+++ gemini-clon/src/routes/AppRoutes.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Chats from "../pages/Chats.jsx";
 import Home from "../pages/Home.jsx";
 import Settings from "../pages/Settings.jsx";
+import AnimatedLayout from '../components/AnimatedLayout.jsx';
+
 
 const AppRoutes = () => {
     return(
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/chats" element={<Chats />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path='*' element={ <h2 className='text-center' >404</h2> } />
+            <Route element={<AnimatedLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/chats" element={<Chats />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path='*' element={ <h2 className='text-center' >404</h2> } />
+            </Route>
         </Routes>
     )
 }
