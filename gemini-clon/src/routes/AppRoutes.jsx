cb11e81b66daf92863c4a266e145e883ee84b048
NIBI <haase.sergio1@gmail.com>
2025-09-20 12:50:20 -0600

    Protegiendo rutas

--- gemini-clon/src/routes/AppRoutes.jsx
+++ gemini-clon/src/routes/AppRoutes.jsx
@@ -1,17 +1,32 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "../context/Context.jsx";
+import AnimatedLayout from '../components/AnimatedLayout.jsx';
 import Chats from "../pages/Chats.jsx";
 import Home from "../pages/Home.jsx";
 import Settings from "../pages/Settings.jsx";
-import AnimatedLayout from '../components/AnimatedLayout.jsx';
+import AuthForm from "../auth/AuthForm.jsx";
+import AuthForm2 from "../auth/AuthForm2.jsx";
+
+
 
 
 const AppRoutes = () => {
+
+        const {user} = useContext(Context)
+
     return(
         <Routes>
             <Route element={<AnimatedLayout />}>
                 <Route path="/" element={<Home />} />
-                <Route path="/chats" element={<Chats />} />
-                <Route path="/settings" element={<Settings />} />
+                <Route path="/1" element={<AuthForm />} />
+                <Route path="/auth" element={<AuthForm2 />} />
+                <Route path="/chats" element={
+                    user ? <Chats /> : <Navigate to="/auth" />
+                } />
+                <Route path="/settings" element={
+                    user ? <Settings /> : <Navigate to="/auth" />
+                } />
                 <Route path='*' element={ <h2 className='text-center' >404</h2> } />
             </Route>
         </Routes>
