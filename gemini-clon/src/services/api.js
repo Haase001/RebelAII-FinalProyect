508bb13927a8b80deae59fab18fe5bc5a554516f
NIBI <haase.sergio1@gmail.com>
2025-09-23 20:59:16 -0600

    Creacion appi.js para conexion con backend

---
+++ gemini-clon/src/services/api.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'http://localhost:4000/api';
+
+export const registerUser = async (userData) => {
+    const res = await fetch(`${BASE_URL}/auth/signup`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(userData),
+    });
+
+    return res.json();
+};
+
+export const loginUser = async (credentials) => {
+    const res = await fetch(`${BASE_URL}/auth/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(credentials),
+    });
+
+    return res.json();
+};
