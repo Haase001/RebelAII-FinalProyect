d9ce6f8eb22eb1931187a9dd7a4d11b2798659c7
NIBI <haase.sergio1@gmail.com>
2025-09-23 22:28:57 -0600

    feat: se agrego la foncionque verifica si el correo ya está registrado

--- gemini-clon/src/services/api.js
+++ gemini-clon/src/services/api.js
@@ -1,5 +1,16 @@
 const BASE_URL = 'http://localhost:4000/api';
 
+export const checkEmailExists = async (email) => {
+    const res = await fetch(`${BASE_URL}/auth/check-email`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email }),
+    });
+
+    return res.json();
+};
+
+
 export const registerUser = async (userData) => {
     const res = await fetch(`${BASE_URL}/auth/signup`, {
         method: 'POST',
